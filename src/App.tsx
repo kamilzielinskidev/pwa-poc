ec2bfd8e97bb4ae0ed0d279a34c6f1a6e0edbcaf
kamilzielinskidev <kamilzielinskidev@gmail.com>
2022-07-12 20:48:30 +0200

    initial pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { get, set } from 'idb-keyval';
+import { FC, useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useBoolean, useEventListener } from 'usehooks-ts';
 
-function App() {
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import { Box, Button, Container, IconButton, TextField } from '@mui/material';
+
+import { resizeImage } from './resizer';
+
+type Lambda<A, B> = (a: A) => B;
+
+const Login: FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ mt: "8rem" }}>
+      <Box
+        component="form"
+        sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+      >
+        <TextField label="Login" variant="outlined" />
+        <TextField label="Password" variant="outlined" />
+        <Button
+          variant="outlined"
+          onClick={() => {
+            navigate("/pictures");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          login
+        </Button>
+      </Box>
+    </Box>
   );
-}
+};
 
-export default App;
+const PicturePreload: FC<{ pictures: File[] }> = ({ pictures }) => {
+  return (
+    <Box sx={{ border: "1px solid #4dd" }}>
+      {pictures.map((picture) => {
+        return (
+          <img
+            key={`${picture.name} ${picture.size}`}
+            width="200"
+            height="200"
+            alt=""
+            src={URL.createObjectURL(picture)}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+const PicturesUpload: FC = () => {
+  const [picturesToSync, setPicturesToSync] = useState<File[]>([]);
+  const { value: isSyncing, setValue: setIsSyncing } = useBoolean(false);
+  const { value: isOnline, setValue: setIsOnline } = useBoolean(
+    navigator.onLine
+  );
+
+  useEffect(() => {
+    get("files").then((files) => {
+      if (files !== undefined && files.length > 0) {
+        setPicturesToSync(files);
+      }
+    });
+  }, []);
+
+  useEventListener("offline", () => {
+    setIsOnline(false);
+  });
+
+  useEventListener("online", () => {
+    setIsOnline(true);
+  });
+
+  useEventListener("online", () => {
+    if (picturesToSync) {
+      setIsSyncing(true);
+      new Promise((ok) => {
+        setTimeout(() => {
+          console.log("UPLOADING");
+          picturesToSync.forEach((picture) => {
+            console.log(picture.name);
+          });
+          setPicturesToSync([]);
+          set("files", []);
+          ok(null);
+        }, 500);
+      }).then(() => {
+        setIsSyncing(false);
+      });
+    }
+  });
+
+  return (
+    <Box>
+      {isOnline ? "You are online" : "You are offline"}
+      <IconButton color="primary" component="label">
+        <input
+          hidden
+          type="file"
+          accept="image/*"
+          capture="environment"
+          onChange={(e) => {
+            const picturesList = e.target.files;
+            const picture = picturesList?.item(0);
+            if (picture !== null && picture !== undefined) {
+              get("files").then((files) => {
+                if (files !== undefined && files.length > 0) {
+                  set("files", [...files, picture]);
+                } else {
+                  set("files", [picture]);
+                }
+              });
+
+              resizeImage({ file: picture, maxSize: 200 }).then(
+                (resizedPicture) => {
+                  setPicturesToSync((pictures) => [
+                    ...pictures,
+                    resizedPicture,
+                  ]);
+                }
+              );
+            }
+          }}
+        />
+        <PhotoCamera />
+      </IconButton>
+      <PicturePreload pictures={picturesToSync} />
+      {isSyncing && <span>IS SYNCING...</span>}
+    </Box>
+  );
+};
+
+export const App: FC = () => {
+  return (
+    <Container maxWidth="sm">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="pictures" element={<PicturesUpload />} />
+      </Routes>
+    </Container>
+  );
+};
---
+++ src/resizer.ts
@@ -0,0 +1,57 @@
+type IResizeImageOptions = {
+  maxSize: number;
+  file: File;
+};
+
+export const resizeImage = (settings: IResizeImageOptions) => {
+  const file = settings.file;
+  const maxSize = settings.maxSize;
+  const reader = new FileReader();
+  const image = new Image();
+  const canvas = document.createElement("canvas");
+  const dataURItoBlob = (dataURI: string) => {
+    const bytes =
+      dataURI.split(",")[0].indexOf("base64") >= 0
+        ? atob(dataURI.split(",")[1])
+        : unescape(dataURI.split(",")[1]);
+    const max = bytes.length;
+    const ia = new Uint8Array(max);
+    for (var i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+    return new File([ia], file.name);
+  };
+  const resize = () => {
+    let width = image.width;
+    let height = image.height;
+
+    if (width > height) {
+      if (width > maxSize) {
+        height *= maxSize / width;
+        width = maxSize;
+      }
+    } else {
+      if (height > maxSize) {
+        width *= maxSize / height;
+        height = maxSize;
+      }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext("2d")!.drawImage(image, 0, 0, width, height);
+    let dataUrl = canvas.toDataURL("image/jpeg");
+    return dataURItoBlob(dataUrl);
+  };
+
+  return new Promise<File>((ok, no) => {
+    if (!file.type.match(/image.*/)) {
+      no(new Error("Not an image"));
+      return;
+    }
+
+    reader.onload = (readerEvent: any) => {
+      image.onload = () => ok(resize());
+      image.src = readerEvent.target.result;
+    };
+    reader.readAsDataURL(file);
+  });
+};
